c264a41aa2e4724b27169e8d83ac3b688bb86f06
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-08-27 18:59:13 +0200

    Added values

--- settings.py
+++ settings.py
@@ -2,3 +2,6 @@ LATITUDE = 10.0
 LONGITUDE = 10.0
 HOURS_AHEAD = 12
 TIMEZONE = "Continent/City"
+API_KEY = "your_key"
+BASE = "https://dmigw.govcloud.dk/v1/forecastedr/collections/harmonie_dini_sf/position"
+CANDIDATE = "total-precipitation"
